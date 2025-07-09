cc862b8308351804ae9ec32c061068862098491c
Illya-01 <illyaroman103@gmail.com>
2025-07-09 18:12:15 +0300

    fix: change relative positioning to absolute for consistency in Ping component

--- src/components/Ping.tsx
+++ src/components/Ping.tsx
@@ -6,7 +6,7 @@ const Ping = () => {
       <div className="absolute -left-4 top-1">
         <span className="flex size-[11px]">
           <span className="absolute inline-flex h-full w-full animate-ping bg-primary/75 rounded-full"></span>
-          <span className="relative inline-flex h-full w-full bg-primary/75 rounded-full"></span>
+          <span className="absolute inline-flex h-full w-full bg-primary/75 rounded-full"></span>
         </span>
       </div>
     </div>
